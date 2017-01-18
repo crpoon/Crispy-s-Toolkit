dadc811260a229a20a454d1093be5ede6ca706f4
Christopher Poon <crpoon@gmail.com>
2017-01-17 19:14:46 -0800

    Added JSON parser for the data + did the damage UI

--- app/src/main/java/com/crpoon/crispytoolkit/freezemage/ui/FreezeMageActivity.java
+++ app/src/main/java/com/crpoon/crispytoolkit/freezemage/ui/FreezeMageActivity.java
@@ -2,6 +2,7 @@ package com.crpoon.crispytoolkit.freezemage.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +15,17 @@ import com.crpoon.crispytoolkit.freezemage.data.DamageSpell;
 import com.crpoon.crispytoolkit.freezemage.data.ItemGroup;
 import com.crpoon.crispytoolkit.freezemage.data.SpellDamageMinion;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +33,9 @@ import java.util.List;
 public class FreezeMageActivity extends AppCompatActivity {
 
     // Constants
+    protected final String LOG_TAG = this.getClass().getSimpleName();
     protected final String DAMAGE_SPELLS = "Direct Damage Spells";
     protected final String SPELL_DAMAGE_MINIONS = "Spell Damage Minions";
-    protected final String TEXT_COLOR = "#FFFFFFFF";
 
     private ItemsExpandableListAdaptor listAdaptor;
     private ExpandableListView listView;
@@ -81,30 +93,75 @@ public class FreezeMageActivity extends AppCompatActivity {
     }
 
     private void loadData() {
-        // TODO: To implement JSON parser and import real data
-
         // First, we need to add our Groups
         ItemGroup damageSpellGroup = new ItemGroup(DAMAGE_SPELLS);
         ItemGroup spellDamageGroup = new ItemGroup(SPELL_DAMAGE_MINIONS);
-        // TODO: Add cards to each group
 
-        // Start of temporary code
-        List<Card> damageSpells = new ArrayList<>();
-        Card fireball = new DamageSpell("Fireball", 6);
-        damageSpells.add(fireball);
-        Card frostbolt = new DamageSpell("Frostbolt", 3);
-        damageSpells.add(frostbolt);
+        // Second, we need to get our cards from the JSON data
+        List<Card> damageSpells = getDamageSpells();
+        List<Card> spellDamageMinions = getSpellDamageMinions();
 
-        List<Card> spellDamageMinions = new ArrayList<>();
-        Card bloodmageThalnos = new SpellDamageMinion("Bloodmage Thalnos", 1);
-        spellDamageMinions.add(bloodmageThalnos);
-
+        // Third, we have to add the cards to the parent groups
         damageSpellGroup.setCards(damageSpells);
         spellDamageGroup.setCards(spellDamageMinions);
-        // End of temporary code
 
         itemGroups.add(damageSpellGroup);
         itemGroups.add(spellDamageGroup);
+    }
+
+    private List<Card> getDamageSpells() {
+        List<Card> damageSpells = new ArrayList<>();
+        String jsonString = getJsonFromResources(R.raw.damage_spell);
+        try {
+            JSONArray spellArr = new JSONArray(jsonString);
+            for (int i = 0; i < spellArr.length(); i++) {
+                JSONObject jsonSpell = spellArr.getJSONObject(i);
+                String name = jsonSpell.getString("name");
+                int damage = jsonSpell.getInt("baseDamage");
+                DamageSpell spell = new DamageSpell(name, damage);
+                damageSpells.add(spell);
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Exception when parsing JSON", e);
+        }
+        return damageSpells;
+    }
+
+    private List<Card> getSpellDamageMinions() {
+        List<Card> minions = new ArrayList<>();
+        String jsonString = getJsonFromResources(R.raw.spell_damage);
+        try {
+            JSONArray minionArr = new JSONArray(jsonString);
+            for (int i = 0; i < minionArr.length(); i++) {
+                JSONObject jsonSpell = minionArr.getJSONObject(i);
+                String name = jsonSpell.getString("name");
+                int spellDamage = jsonSpell.getInt("spellDamage");
+                SpellDamageMinion minion = new SpellDamageMinion(name, spellDamage);
+                minions.add(minion);
+            }
+        } catch (JSONException e) {
+            Log.e(LOG_TAG, "Exception when parsing JSON", e);
+        }
+        return minions;
+    }
+
+    private String getJsonFromResources(int id) {
+        InputStream is = getResources().openRawResource(id);
+        Writer sw = new StringWriter();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line = reader.readLine();
+            while (line != null) {
+                sw.write(line);
+                line = reader.readLine();
+            }
+        } catch (UnsupportedEncodingException e) {
+            Log.e(LOG_TAG, "Exception when reading JSON", e);
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "Exception when reading JSON", e);
+        }
+
+        return sw.toString();
     }
 
     private void expandOrCollapseAll(boolean expand) {
